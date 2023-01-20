4a18472bea05bbfce9a48b7091a26b5f47526670
go <xxxviii00@gmail.com>
2023-01-20 15:18:08 +0900

    chore: semantic release 설정 업데이트

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,16 @@ module.exports = {
   branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
+    {
+      releaseRules: [
+        { breaking: true, release: "major" },
+        { revert: true, release: "patch" },
+        { type: "feat", release: "minor" },
+        { type: "fix", release: "patch" },
+        { type: "✨feat", release: "minor" },
+        { type: "🐛fix", release: "patch" },
+      ],
+    },
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
