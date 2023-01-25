e19dc78957b686e8d2cd80365a6a555b2ecc2a4a
go <xxxviii00@gmail.com>
2023-01-25 22:41:25 +0900

    fix: global style이 적용되지 않는 이슈 처리
    * index에서 렌더링하는 컴포넌트 변경

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Root from 'Root';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(<Root />);
