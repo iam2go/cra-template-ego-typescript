ac8ed9e28e44a0b14ce378a1c8c55a409e3e360f
go <xxxviii00@gmail.com>
2023-02-08 23:11:19 +0900

    chore: commit convention 변경 반영

--- .releaserc.js
+++ .releaserc.js
@@ -2,15 +2,6 @@ module.exports = {
   branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    {
-      parserOpts: {
-        parserOpts: {
-          headerPattern:
-            /^(?<emoji>:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]gi)?\s*(?<type>\w+)?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?(?<ticket>#\d*)\)?)?$/,
-          headerCorrespondence: ["emoji", "type", "subject", "ticket"],
-        },
-      },
-    },
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^(?<emoji>:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]gi)?\s*(?<type>\w+)?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?(?<ticket>#\d*)\)?)?$/,
-      headerCorrespondence: ["emoji", "type", "subject", "ticket"],
-    },
-  },
   rules: {
     "type-enum": [
       2,
       "always",
-      ["chore", "docs", "feat", "fix", "refactor", "revert", "style", "test"],
+      ["chore", "docs", "feat", "fix", "refactor", "revert", "style"],
     ],
   },
 };
